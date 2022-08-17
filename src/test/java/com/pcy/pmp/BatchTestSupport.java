518dcb4b7602c80961dee56ed4a230aeffc90815
Park, Chan-Yu <pcy4196@naver.com>
2022-08-17 22:21:51 +0900

    2022.08.17 commit
    + SpringBatch TEST 공통 구현
    1. BatchTestSupport.java 
    2. application.yml(Test) 구현

---
+++ src/test/java/com/pcy/pmp/BatchTestSupport.java
@@ -0,0 +1,54 @@
+package com.pcy.pmp;
+
+import com.pcy.pmp.message.MessageRepository;
+import com.pcy.pmp.point.PointRepository;
+import com.pcy.pmp.point.reservation.PointReservationRepository;
+import com.pcy.pmp.point.wallet.PointWalletRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBatchTest
+@ActiveProfiles("test")
+public abstract class BatchTestSupport {
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    PointRepository pointRepository;
+
+    @Autowired
+    PointWalletRepository pointWalletRepository;
+
+    @Autowired
+    PointReservationRepository pointReservationRepository;
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    protected JobExecution launchJob(Job job, JobParameters jobParameters) throws Exception {
+        JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+        jobLauncherTestUtils.setJob(job);
+        jobLauncherTestUtils.setJobLauncher(jobLauncher);
+        jobLauncherTestUtils.setJobRepository(jobRepository);
+        return jobLauncherTestUtils.launchJob(jobParameters);
+    }
+
+    @AfterEach
+    protected void deleteAll() {
+        pointRepository.deleteAll();
+        pointWalletRepository.deleteAll();
+        pointReservationRepository.deleteAll();
+        messageRepository.deleteAll();
+    }
+}
