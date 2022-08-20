731966635a218cde8e84f69b65b0aea41e9c1ab4
Park, Chan-Yu <pcy4196@naver.com>
2022-08-21 00:45:00 +0900

    2022.08.21 commit  -- 001
    1. BatchTestSupport.java 파일 변경
        (@SpringBatchTest -> @SpringBootTest)

--- src/test/java/com/pcy/pmp/BatchTestSupport.java
+++ src/test/java/com/pcy/pmp/BatchTestSupport.java
@@ -11,13 +11,14 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBatchTest
+@SpringBootTest
 @ActiveProfiles("test")
 public abstract class BatchTestSupport {
+
     @Autowired
     private JobLauncher jobLauncher;
 
